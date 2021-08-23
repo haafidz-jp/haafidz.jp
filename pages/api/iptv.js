ad8fb9b927370d43ef236f7c19d5bbe43dec9b45
Haafidz Nurul Salim <haafidz123@gmail.com>
2021-08-24 00:28:06 +0700

    contoh api.

---
+++ pages/api/iptv.js
@@ -0,0 +1,10 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function IptvAPI(req, res) {
+    res.status(200).json(
+        {
+        name: 'Next.js',
+        body: 'none',
+        
+    })
+}
